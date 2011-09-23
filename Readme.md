68c87af6340ddc4134bb8817e0456c0ffd5c9ac8
Stephen Johnson <srj15@case.edu>
2011-09-23 16:04:20 -0400

    Hello gh-pages

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This gh-pages branch has the master branch added as a submodule with Github
+as the remote. This is because the pages are generated from data in the master
+branch and it's a pain in the ass to keep two clones.
---
+++ generate_html.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import yaml
+
+data = []
+for path in os.listdir('master_branch'):
+    if os.path.isdir(path) and path.startswith('2'):
+        try:
+            with open(os.path.join(path, 'info.yaml')):
+                pass
+        except IOError:
+            print('{0} does not have an info.yaml file and will not appear on the page.'.format(path))

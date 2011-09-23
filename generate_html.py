9e050e539e8355ee30c2ee42b42b5c9e3c4db54d
Stephen Johnson <srj15@case.edu>
2011-09-23 18:13:06 -0400

    Links to audio, video, presentation

--- generate_html.py
+++ generate_html.py
@@ -1,4 +1,6 @@
+from collections import OrderedDict
 import datetime
+import glob
 import os
 import sys
 
@@ -12,17 +14,22 @@ for path in os.listdir('.'):
         date = datetime.datetime.strptime(date_str, '%Y%m%d').strftime('%B %d, %Y')
         try:
             with open(os.path.join(path, 'info.yaml')) as f:
-                unsorted_data[path] = yaml.load(f)
-                unsorted_data[path]['title'] = title
-                unsorted_data[path]['date'] = date
+                d = yaml.load(f)
+                unsorted_data[path] = d
+                d['title'] = title
+                d['date'] = date
+                d.setdefault('audio', None)
+                d.setdefault('video', None)
+                d['formats'] = OrderedDict()
+                for prespath in glob.glob(os.path.join(path, "presentation.*")):
+                    fmt = os.path.splitext(prespath)[1][1:]
+                    d['formats'][fmt] = os.path.join(path, prespath)
         except IOError:
             print('{0} does not have an info.yaml file and will not appear on the page.'.format(path))
 
-sorted_data = {k: unsorted_data[k] for k in sorted(unsorted_data.keys())}
+sorted_data = OrderedDict((k, unsorted_data[k]) for k in sorted(unsorted_data.keys()))
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
-
 t = env.get_template('template.html')
-
 with open('index.html', 'w') as f:
     f.write(t.render(talks=sorted_data))

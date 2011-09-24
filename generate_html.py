672879cbb23c65307aa281fdf075af9187815ad8
Stephen Johnson <srj15@case.edu>
2011-09-24 10:11:30 -0400

    Refactoring

--- generate_html.py
+++ generate_html.py
@@ -9,6 +9,7 @@ import jinja2
 import yaml
 
 GIT_RE = re.compile(r'\s+url = git://github.com/hacsoc/(?P<name>.*)\.git\n')
+PATH_RE = re.compile(r'\s+path = (?P<path>.*)\n')
 
 def submodule_name_map():
     submodules = {}
@@ -16,50 +17,54 @@ def submodule_name_map():
     with open('.gitmodules', 'r') as f:
         k = None
         for line in f:
-            m = GIT_RE.match(line)
-            if m:
-                submodules[k] = m.group('name')
+            gm = GIT_RE.match(line)
+            pm = PATH_RE.match(line)
+            if gm:
+                submodules[k] = gm.group('name')
                 k = None
-            elif 'path' in line:
-                k = line.split('path = ', 1)[1][:-1]
+            elif pm:
+                k = pm.group('path')
 
     return submodules
 
-def parse_info_yaml(path):
-    with open(path, 'r') as f:
+SUBMODULE_NAMES = submodule_name_map()
+
+def read_data_from_dir(path):
+    date_str, title = path.split(' ', 1)
+    date_obj = datetime.datetime.strptime(date_str, '%Y%m%d')
+    with open(os.path.join(path, 'info.yaml'), 'r') as f:
         d = yaml.load(f)
         d.setdefault('audio', None)
         d.setdefault('video', None)
         d.setdefault('links', [])
+    d['title'] = title
+    d['date'] = date_obj.strftime('%B %d, %Y')
+    d['date_obj'] = date_obj
+    d['formats'] = formats_at_path(path)
+    if path in SUBMODULE_NAMES:
+        d['github'] = 'http://www.github.com/hacsoc/{0}'.format(SUBMODULE_NAMES[path])
+    else:
+        d['github'] = 'http://www.github.com/hacsoc/talks/tree/master/{0}'.format(path)
     return d
 
+def formats_at_path(path):
+    formats = OrderedDict()
+    for prespath in glob.glob(os.path.join(path, "presentation.*")):
+        fmt = os.path.splitext(prespath)[1][1:]
+        formats[fmt] = os.path.join(path, prespath)
+    return formats
+
 def data_for_repos():
-    submodules = submodule_name_map()
-    unsorted_data = {}
+    unsorted_data = []
     for path in os.listdir('.'):
         if os.path.isdir(path) and path.startswith('2'):
-            date_str, title = path.split(' ', 1)
-            date_obj = datetime.datetime.strptime(date_str, '%Y%m%d')
             try:
-                d = parse_info_yaml(os.path.join(path, 'info.yaml'))
-                unsorted_data[path] = d
-                d['title'] = title
-                d['date'] = date_obj.strftime('%B %d, %Y')
-                d['date_obj'] = date_obj
-                d['formats'] = OrderedDict()
-                for prespath in glob.glob(os.path.join(path, "presentation.*")):
-                    fmt = os.path.splitext(prespath)[1][1:]
-                    d['formats'][fmt] = os.path.join(path, prespath)
-                if path in submodules:
-                    d['github'] = 'http://www.github.com/hacsoc/{0}'.format(submodules[path])
-                else:
-                    d['github'] = 'http://www.github.com/hacsoc/talks/tree/master/{0}'.format(path)
+                unsorted_data.append(read_data_from_dir(path))
             except IOError:
                 print('{0} does not have an info.yaml file and will not appear on the page.'.format(path))
 
-    key = lambda d: unsorted_data[d]['date_obj']
-    return [unsorted_data[k] for k in sorted(unsorted_data.keys(),
-                                             key=key, reverse=True)]
+    key = lambda d: d['date_obj']
+    return sorted(unsorted_data, key=key, reverse=True)
 
 def render_talks(talks):
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))

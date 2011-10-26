21d5b84554944c07f53f071cb2b54fe7f992a9fd
Tim Henderson <tim.tadh@gmail.com>
2011-10-26 16:54:48 -0400

    Higher Order Functions (curry example)
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ 20111026 Higher Order Functions/ex/closure/curry.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Tim Henderson
+# Email: tim.tadh@hackthology.com
+# This file is in the Public Domain
+
+import functools
+
+def add(a, b): return a + b
+def sub(a, b): return a - b
+def mul(a, b): return a * b
+def div(a, b): return a / b
+
+def Acc(init, step):
+    acc = [init]
+    def Acc(op):
+        acc[0] = op(acc[0], step)
+        return acc[0]
+    return Acc
+
+acc = Acc(5, 3)
+
+
+def Curry1(f, a):
+    def wrapper():
+        return f(a)
+    return wrapper
+
+addacc = Curry1(acc, add)
+subacc = functools.partial(acc, sub) # no boilerplate
+
+print acc(mul)
+print addacc()
+print addacc()
+print addacc()
+print acc(div)
+print subacc()
+
